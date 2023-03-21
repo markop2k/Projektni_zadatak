e77b7ccb82f422fa422c7057f40f6201ddd411c5
Marko Pavlek <mpavlek@tvz.hr>
2023-03-21 19:50:12 +0100

    Projektni_zadatak

---
+++ PZ1.py
@@ -0,0 +1,34 @@
+from datetime import date
+korisnik = {}
+korisnik['ime'] = input("Unesite ime korisnik:").capitalize()
+korisnik['prezime'] = input("Unesite prezime korisnik:").capitalize()
+korisnik['telefon'] = int(input("Unesite telefon korisnika: "))
+korisnik['email'] = input("Unesite email korisnika:").strip()
+#print("Kolegij",kolegij['predmet'],"nosi",kolegij['ects'],"ECTS bodova.")
+
+artikl = {}
+artikl['naslov'] = input("Unesite naslov artikla:")
+artikl['opis'] = input("Unesite opis artikla:")
+artikl['cijenu'] = float(input("Unesite cijenu artikla:"))
+
+
+prodaja = {}
+prodaja['korisnik'] = korisnik
+prodaja['artikl'] = artikl
+dan= int(input("Unesite dan isteka prodaje:"))
+mjesec= int(input("Unesite mjesec isteka prodaje:"))
+godina= int(input("Unesite godinu isteka prodaje: "))
+prodaja['datum'] = date(godina,mjesec,dan)
+
+print(f"Informacije o artiklu: "
+      f"\nNaslov:{prodaja['artikl']['naslov']} "
+      f"\nOpis:{prodaja['artikl']['opis']} "
+      f"\nCijena:{prodaja['artikl']['cijena']} "
+      f"\nDatum isteka prodaje: "
+      f"\nDan:{prodaja['datum'].day}"
+      f"\nMjesec:{prodaja['datum'].month} "
+      f"\nGodina:{prodaja['datum'].year} "
+      f"\n Informacije o korisniku: "
+      f"\n{prodaja['korisnik']['ime']}{prodaja['korisnik']['prezime']}"
+      f"\nTelefon:{prodaja['korisnik']['telefon']}"
+      f"\nEmail:{prodaja['korisnik']['email']}")

d59ba5812aded5c5902a3fed2fc5887ca2a11140
markop2k <mpavlek@tvz.hr>
2023-03-22 12:32:24 +0100

    zadatak

--- Programski_zadatak.py
+++ Programski_zadatak.py
@@ -1,34 +1,33 @@
 from datetime import date
 korisnik = {}
-korisnik['ime'] = input("Unesite ime korisnik:").capitalize()
-korisnik['prezime'] = input("Unesite prezime korisnik:").capitalize()
+korisnik['ime'] = input("Unesite ime korisnik: ").capitalize()
+korisnik['prezime'] = input("Unesite prezime korisnik: ").capitalize()
 korisnik['telefon'] = int(input("Unesite telefon korisnika: "))
-korisnik['email'] = input("Unesite email korisnika:").strip()
-#print("Kolegij",kolegij['predmet'],"nosi",kolegij['ects'],"ECTS bodova.")
+korisnik['email'] = input("Unesite email korisnika: ").strip()
 
 artikl = {}
-artikl['naslov'] = input("Unesite naslov artikla:")
-artikl['opis'] = input("Unesite opis artikla:")
-artikl['cijena'] = float(input("Unesite cijenu artikla:"))
+artikl['naslov'] = input("Unesite naslov artikla: ")
+artikl['opis'] = input("Unesite opis artikla: ")
+artikl['cijena'] = float(input("Unesite cijenu artikla: "))
 
 
 prodaja = {}
 prodaja['korisnik'] = korisnik
 prodaja['artikl'] = artikl
-dan= int(input("Unesite dan isteka prodaje:"))
-mjesec= int(input("Unesite mjesec isteka prodaje:"))
+dan= int(input("Unesite dan isteka prodaje: "))
+mjesec= int(input("Unesite mjesec isteka prodaje: "))
 godina= int(input("Unesite godinu isteka prodaje: "))
 prodaja['datum'] = date(godina,mjesec,dan)
 
 print(f"Informacije o artiklu: "
-      f"\nNaslov:{prodaja['artikl']['naslov']} "
-      f"\nOpis:{prodaja['artikl']['opis']} "
-      f"\nCijena:{prodaja['artikl']['cijena']} "
+      f"\n\t\tNaslov: {prodaja['artikl']['naslov']} "
+      f"\n\t\tOpis: {prodaja['artikl']['opis']} "
+      f"\n\t\tCijena: {prodaja['artikl']['cijena']} "
       f"\nDatum isteka prodaje: "
-      f"\nDan:{prodaja['datum'].day}"
-      f"\nMjesec:{prodaja['datum'].month} "
-      f"\nGodina:{prodaja['datum'].year} "
+      f"\n\t\tDan: {prodaja['datum'].day}"
+      f"\n\t\tMjesec: {prodaja['datum'].month} "
+      f"\n\t\tGodina: {prodaja['datum'].year} "
       f"\nInformacije o korisniku: "
-      f"\n{prodaja['korisnik']['ime']} {prodaja['korisnik']['prezime']}"
-      f"\nTelefon:{prodaja['korisnik']['telefon']}"
-      f"\nEmail:{prodaja['korisnik']['email']}")
+      f"\n\t\t{prodaja['korisnik']['ime']} {prodaja['korisnik']['prezime']}"
+      f"\n\t\tTelefon: {prodaja['korisnik']['telefon']}"
+      f"\n\t\tEmail: {prodaja['korisnik']['email']}")
